df5c8df1f7c52b07004764bfc49f8e9d4abc4580
ltt <ttli1@chinaums.com>
2019-03-25 14:45:48 +0800

    demo2

---
+++ demo2/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img alt="Netpay logo" src="./assets/logo.png">
+    <Netpay headMsg="欢迎来到网付平台" targetSysName="微信" targetSysNum="10000"/>
+  </div>
+</template>
+
+<script>
+import Netpay from './components/Netpay.vue'
+
+export default {
+  name: 'app',
+  components: {
+      Netpay
+  }
+}
+</script>
