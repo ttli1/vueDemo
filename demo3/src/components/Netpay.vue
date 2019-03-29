501dbae110c0158f5e0909fd0d5158c532580057
ltt <ttli1@chinaums.com>
2019-03-29 13:57:26 +0800

    rename

--- demo3/src/components/Netpay.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <b>欢迎来到网付平台</b>
-</template>
-
-<script>
-export default {
-  name: 'Netpay'
-}
-</script>
--- demo3/src/router/routers.js
+++
@@ -1,7 +0,0 @@
-import Merchant from '../views/Merchant.vue'
-import Order from '../views/Order.vue'
-
-export default [
-    { path: '/merchant', component: Merchant },
-    { path: '/order', component: Order }
-]
